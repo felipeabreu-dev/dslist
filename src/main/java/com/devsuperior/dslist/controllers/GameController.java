d8259b1bb4187939a9541987fa2d98c4a3003dbb
Felipe Abreu <abreufelipe12@hotmail.com>
2025-01-21 21:42:43 -0300

    GameDTO, Game find by Id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
@@ -22,5 +24,11 @@ public class GameController {
 	public ResponseEntity<List<GameMinDTO>> findAll(){
 		List<GameMinDTO> games = gameService.findAll();
 		return ResponseEntity.ok().body(games);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GameDTO> findById(@PathVariable Long id){
+		GameDTO game = gameService.findById(id);
+		return ResponseEntity.ok().body(game);
 	}
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.repositories.GameRepository;
@@ -15,9 +17,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true) //Indica para o Spring que esse método deve seguir os padrões de uma Transação e parâmetro readOnly indica que não utilizaremos esse método para escrever
 	public List<GameMinDTO> findAll(){
 		List<Game> list = gameRepository.findAll();
 		
 		return list.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id){
+		Game game = gameRepository.findById(id).get();
+		
+		return new GameDTO(game);
 	}
 }
